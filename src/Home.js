f0b56367a9fe5018bc6b6aa95ba05da56d09d5cf
Lauren Clark <lauren.c4231@gmail.com>
2020-07-16 14:14:19 +0100

    REGISTER FORM

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+// 1. Import `useForm` and `usePlugin`
 import { useForm, usePlugin, useCMS } from 'tinacms'
 import { Hero } from './components/Hero'
 import data from './data/data.json'
@@ -6,16 +7,23 @@ import data from './data/data.json'
 export default function Home() {
     const cms = useCMS()
 
-    const myFormConfig = {
-        id: '.data/data.json',
+    // 2. Create a config object
+    const formConfig = {
+        id: './data/data.json',
         initialValues: data,
         onSubmit() {
             cms.alerts.success('Saved!')
         }
     }
+
+    // 3. Create and register the form.
+    const [pageData, form] = useForm(formConfig)
+    usePlugin(form)
+
+    // 4. Use the return data now connected with a TinaCMS form
     return (
         <div className="home">
-            <Hero {...data} />
+            <Hero {...pageData} />
         </div>
     )
 }
