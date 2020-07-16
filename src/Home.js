f84884e835e1166b024071f9968889ea39e5295f
Lauren Clark <lauren.c4231@gmail.com>
2020-07-16 14:17:34 +0100

    Import the inline form and wrap Hero in it, pass form in as prop

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,8 @@
 import React from 'react'
 // 1. Import `useForm` and `usePlugin`
 import { useForm, usePlugin, useCMS } from 'tinacms'
+// 5. Import `InlineForm`
+import { InlineForm } from 'react-tinacms-inline'
 import { Hero } from './components/Hero'
 import data from './data/data.json'
 
@@ -16,14 +18,17 @@ export default function Home() {
         }
     }
 
-    // 3. Create and register the form.
+    // 3. Create and register the form. (Getting pageData, and form from useForm - can name them however but must be in order)
     const [pageData, form] = useForm(formConfig)
     usePlugin(form)
 
     // 4. Use the return data now connected with a TinaCMS form
     return (
+        // 6. Wrap `InlineForm` around `Hero`, pass the form
         <div className="home">
-            <Hero {...pageData} />
+            <InlineForm form={form}>
+                <Hero {...pageData} />
+            </InlineForm>
         </div>
     )
 }
